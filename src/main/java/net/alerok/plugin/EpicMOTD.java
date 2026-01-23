d346cd6d65c4c2a628f8d158ddd0ee6f5802c2b4
Victor Wolff <contact@alerok.net>
2026-01-23 15:06:40 -0300

    refactor: changing template default settings

--- src/main/java/net/alerok/plugin/EpicMOTD.java
+++ src/main/java/net/alerok/plugin/EpicMOTD.java
@@ -1,4 +1,4 @@
-package org.example.plugin;
+package net.alerok.plugin;
 
 import com.hypixel.hytale.logger.HytaleLogger;
 import com.hypixel.hytale.server.core.plugin.JavaPlugin;
@@ -6,15 +6,11 @@ import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
 
 import javax.annotation.Nonnull;
 
-/**
- * This class serves as the entrypoint for your plugin. Use the setup method to register into game registries or add
- * event listeners.
- */
-public class ExamplePlugin extends JavaPlugin {
+public class EpicMOTD extends JavaPlugin {
 
     private static final HytaleLogger LOGGER = HytaleLogger.forEnclosingClass();
 
-    public ExamplePlugin(@Nonnull JavaPluginInit init) {
+    public EpicMOTD(@Nonnull JavaPluginInit init) {
         super(init);
         LOGGER.atInfo().log("Hello from " + this.getName() + " version " + this.getManifest().getVersion().toString());
     }
--- src/main/java/net/alerok/plugin/ExampleCommand.java
+++ src/main/java/net/alerok/plugin/ExampleCommand.java
@@ -1,4 +1,4 @@
-package org.example.plugin;
+package net.alerok.plugin;
 
 import com.hypixel.hytale.protocol.GameMode;
 import com.hypixel.hytale.server.core.Message;
